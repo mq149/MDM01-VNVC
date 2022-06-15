8442b6e920d924fa5f3be1c246c056b210bcab42
Bui Minh Quan <32923349+mq149@users.noreply.github.com>
2022-06-15 18:01:54 +0700

    Add Cors for backend API.
    Create vaccine price list page with AJAX call to get data.
    Add some page styling.
    Remove unnecessary code in VaccinePriceListController.

--- vnvc/pages/vaccine-price-list/index.js
+++ vnvc/pages/vaccine-price-list/index.js
@@ -1,3 +1,38 @@
 $(function () {
-  console.log("hello world");
+    console.log("hello world");
+
+    const vaccine_price_list_api = "https://localhost:5001/vaccinepricelist";
+
+    $.ajax({
+        url: vaccine_price_list_api,
+        method: "get",
+        success: function (response) {
+            console.log(response);
+            populateVaccinePriceTable(response);
+        },
+        error: function (errors) {
+            console.log(errors);
+        },
+    });
+
+    function populateVaccinePriceTable(vaccines) {
+        vaccines.forEach((vaccine, index) => {
+            let rowHtml = `
+          <tr>
+            <th scope="row">${index + 1}</th>
+            <td>${vaccine["protectAgainst"]}</td>
+            <td class="light-blue">${vaccine["name"]}</td>
+            <td class="light-blue">${vaccine["countryOfOrigin"]}</td>
+            <td class="blue">${vaccine["retailPrice"].toLocaleString(
+                "de-DE"
+            )}</td>
+            <td class="blue">${vaccine["preOrderPrice"].toLocaleString(
+                "de-DE"
+            )}</td>
+            <td class="blue">${vaccine["status"]}</td>
+          </tr>
+          `;
+            $("table#vaccine-price-list tbody").append(rowHtml);
+        });
+    }
 });
