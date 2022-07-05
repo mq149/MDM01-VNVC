b820488c576f6770524d075282eb98585938e9bc
Nguyễn Văn Kha <61898499+vankhanguyen13@users.noreply.github.com>
2022-07-05 18:53:54 +0700

    Create VaccinationRegistrationHistoryController.cs

---
+++ MDM01-VNVC/Controllers/VaccinationRegistrationHistoryController.cs
@@ -0,0 +1,51 @@
+﻿using MDM01_VNVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Neo4j.Driver;
+
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace MDM01_VNVC.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VaccinationRegistrationHistoryController : ControllerBase
+    {
+        private readonly ILogger<VaccinationRegistrationHistoryController> _logger;
+        private readonly IDriver _driver;
+        private readonly IOptions<DocumentDatabaseSettings> settings;
+        private Action<SessionConfigBuilder> sessionConfig;
+
+        public VaccinationRegistrationHistoryController(ILogger<VaccinationRegistrationHistoryController> logger, IOptions<DocumentDatabaseSettings> settings)
+        {
+            _logger = logger;
+            this.settings = settings;
+         }
+
+        [HttpGet]
+        public IEnumerable<VaccinationRegistrationHistory> Get()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("vnvc");
+            var collection = database.GetCollection<VaccinationRegistrationHistory>("vaccines");
+            List<VaccinationRegistrationHistory> historys = collection.Find(s => s.NguoiLienHe_HoTen != null).ToList();
+            return historys;
+        }
+
+
+        [HttpGet("{sdt}")]
+        public IEnumerable<VaccinationRegistrationHistory> SearchForPhoneNumber(string sdt)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("vnvc");
+            var collection = database.GetCollection<VaccinationRegistrationHistory>("vaccines");
+
+            List<VaccinationRegistrationHistory> historys = collection.Find(s => s.NguoiTiem_SDT == sdt).ToList();
+            return historys;
+        }
+    }
+}
