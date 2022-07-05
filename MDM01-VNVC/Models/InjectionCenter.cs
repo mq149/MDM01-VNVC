8b7a74f5eb588462dd5abc052cc67323d14dc78a
Nguyễn Văn Kha <61898499+vankhanguyen13@users.noreply.github.com>
2022-07-05 18:54:05 +0700

    Create InjectionCenter.cs

---
+++ MDM01-VNVC/Models/InjectionCenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MDM01_VNVC.Models
+{
+    public class InjectionCenter
+    {
+        //[BsonId]
+        //public ObjectId Id { get; set; }
+
+        [BsonElement("TenTT")]
+        public string TenTT { get; set; }
+
+        [BsonElement("DiaChi")]
+        public string DiaChi { get; set; }
+
+        public InjectionCenter(string TenTT, string DiaChi)
+        {
+            this.TenTT = TenTT;
+            this.DiaChi = DiaChi;
+        }
+    }
+}
